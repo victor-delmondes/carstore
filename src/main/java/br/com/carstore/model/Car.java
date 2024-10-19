f9f0264bbee184c3739f535ae4f0749f24caaeb0
Victor Delmondes <v.emanuel2013@gmail.com>
2024-10-19 14:13:46 -0300

    subindo o repositorio

---
+++ src/main/java/br/com/carstore/model/Car.java
@@ -0,0 +1,15 @@
+package br.com.carstore.model;
+
+public class Car {
+
+    private String name;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
